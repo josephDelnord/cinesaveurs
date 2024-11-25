115f3ebb0ee3deb2c23d5b8333685a031d3db486
Esispadion <esispadion@hotmail.com>
2024-11-25 09:08:46 +0100

    ajout recipe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
 import Popular from './components/Popular/Popular';
 import Recipes from './components/Recipes/Recipes';
-
+import Recipe from './components/Recipe/Recipe';
 
 function App() {
   const [showPopular, setShowPopular] = useState(true);
 
   return (
-    <div className="app">
-      <Header />
-      <Recipes togglePopular={() => setShowPopular(!showPopular)} />
-      {showPopular && <Popular isVisible={showPopular} />
-}
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Recipes togglePopular={() => setShowPopular(!showPopular)} />
+                {showPopular && <Popular isVisible={showPopular} />}
+              </>
+            }
+          />
+          <Route
+            path="/recipe/:id"
+            element={<Recipe />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
